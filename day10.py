22b1bb01f3a4e9b3fc6efd6df1ee6f8fb9593ef1
Asiffarooq26 <102163638+Asiffarooq26@users.noreply.github.com>
2025-07-25 21:59:33 +0530

    day9&10

---
+++ day10.py
@@ -0,0 +1,33 @@
+# mydict={
+#     "Roll_No":150,
+#     "name":"Asif",
+#     "course":"Python",
+#     "Academy":"ASD" }
+# print(mydict)
+# print(type(mydict))
+# # print(mydict["name"])
+# # print(mydict.get("names"))
+# mydict["age"]=31
+# mydict["name"]="Chandhu"
+# # print(mydict)
+# # print(mydict.keys())
+# # print(mydict.values())
+# # print(mydict.items())
+# # print(mydict.clear())
+# # print(mydict)
+# # new_dict=mydict.copy()
+# # mydict["name"]="Rahul"
+# # print(mydict)
+# # print(new_dict)
+# # for key in mydict:
+# #     print(key)
+# # for demo in mydict.values():
+# #     print(demo)
+# for key,value in mydict.items():
+#     print(key,value)
+myset={1,2,2,5,"Python","Python","True",13.5}
+print(myset)
+myset.add("Java")
+print(myset)
+myset.remove(2)
+print(myset)
---
+++ day9.py
@@ -0,0 +1,52 @@
+# mylist=["Rahul",550,13.5,True,"Python"]
+# # list2=[50,90,87,"Python"]
+# print(mylist)
+# print(type(mylist))
+# mylist2=list(("C","C++","Python",30))
+# print(mylist2)
+# print(type(mylist2))
+# print(mylist[4])
+# print(mylist[-4])
+# print(mylist[1:5])
+# mylist[3]=500
+# print(len(mylist))
+# mylist.append("Java")
+# mylist.append("Java")
+# print(mylist)
+# mylist.insert(2,"JavaScript")
+# mylist.extend(list2)
+# print(mylist)
+# final_list=mylist+list2
+# print(final_list)
+# mylist.remove(True)
+# mylist.clear()
+# print(mylist)
+# if "Asif" in mylist:
+#     print("Asif is Present")
+# else:
+#     print("Asif is not Present")
+# mylist.pop(4)
+# print(mylist)
+# L1 = [1, 8, 7, 2, 21, 15,18,89]
+# L1.sort(reverse=True)
+# print(L1)
+
+#Tuples
+# tup=(1,"Python",13.5,True)
+# print(tup[-1])
+# print(type(tup))
+# # tup[1]="Java"
+# # print(tup)
+# print(tup[1:4])
+# tuple1 = (1, 2,4)
+# tuple2 = (3, 4,4)
+# result = tuple1 + tuple2
+# # print(result)
+# result=tuple1*2
+# print(result)
+fruits = ("apple", "banana", "cherry")
+print(fruits)
+(green, yellow, red) = fruits
+print(green)
+print(yellow)
+print(red)
